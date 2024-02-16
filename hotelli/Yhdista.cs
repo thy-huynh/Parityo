92edf4ccc5c2534571081b464f0d8cb1d1d8e37b
sailakivi <esko.sailakivi@edu.keuda.fi>
2024-02-16 14:23:33 +0200

    Hotelli

---
+++ hotelli/Yhdista.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Hotelli
+{
+    internal class Yhdista
+    {
+        private readonly MySqlConnection yhteys = new MySqlConnection("datasource=localhost; port 3306; username=root; password=; database=hotelli");
+
+        public MySqlConnection OtaYhteys()
+        { 
+            return yhteys;
+        }
+
+        public void AvaaYhteys()
+        {
+            if(yhteys.State == System.Data.ConnectionState.Closed)
+            {
+                yhteys.Open();
+            }
+        }
+
+        public void SuljeYhteys()
+        {
+            if (yhteys.State == System.Data.ConnectionState.Open)
+            {
+                yhteys.Close();
+            }
+        }
+    }
+}
