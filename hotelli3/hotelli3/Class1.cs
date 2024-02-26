e746fc3cc3289de182946d1e5dc8754d8c8a82ed
unknown <esko.sailakivi@edu.keuda.fi>
2024-02-26 12:34:33 +0200

    hotelli3

---
+++ hotelli3/hotelli3/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace hotelli3
+{
+    internal class Class1
+    {
+        private readonly MySqlConnection yhteys = new MySqlConnection("datasource=localhost; port=3306;username=root;password=;database=hotelli");
+        
+        public MySqlConnection OtaYhteys() 
+        {
+            return yhteys;
+        }
+
+        public void AvaaYhteys()
+        {
+            if(yhteys.State == ConnectionState.Closed)
+            {
+                yhteys.Open();
+            }
+        }
+
+        public void SuljeYhteys() 
+        {
+            if (yhteys.State == ConnectionState.Open)
+            {
+                yhteys.Close();
+            }
+        }
+    }
+}
