a4f7e88f71fa7f62af7085afffcc3c0433660ff0
unknown <esko.sailakivi@edu.keuda.fi>
2024-02-26 13:10:35 +0200

    hotelli3

--- hotelli3/hotelli3/Form2.cs
+++ hotelli3/hotelli3/Form2.cs
@@ -24,17 +24,20 @@ namespace hotelli3
 
         private void hallitseAsiakkaitaToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            Form3 ahlomake = new Form3();
+            ahlomake.ShowDialog();
         }
 
         private void hallitseVarauksiaToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            Form4 vhlomake = new Form4();
+            vhlomake.ShowDialog();
         }
 
         private void hallitseHuoneitaToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            Form5 hhlomake = new Form5();
+            hhlomake.ShowDialog();
         }
     }
 }
